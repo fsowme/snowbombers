b167e43a246cb972a1dd40cceaa19bb52c58bad4
fsow <fsow@yandex.ru>
2020-12-31 21:12:07 +0300

    Add logging in debug file

--- snowbombers/settings.py
+++ snowbombers/settings.py
@@ -107,6 +107,24 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": BASE_DIR / "debug.log",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
