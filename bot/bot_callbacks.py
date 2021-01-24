2917e96b29f990bfc94dc2822b6212c732f19dfb
Vitalii <fsow@yandex.ru>
2021-01-24 18:42:46 +0300

    Back button in info.

--- bot/bot_callbacks.py
+++ bot/bot_callbacks.py
@@ -12,37 +12,42 @@ def start(update, context):
     )
 
 
-def choose_buttons(update):
-    land = update.callback_query.data
-    if Continent.objects.filter(name=land):
-        return countries_buttons(land)
-    if Country.objects.filter(name=land):
-        return resorts_buttons(land)
-    raise ValueError("Unknown region")
+def choose_buttons(next_step):
+    land_type, land_name = next_step.split(sep=":")
+    if land_type == "start_info":
+        return continents_buttons()
+    if land_type == "continent":
+        return countries_buttons(land_name)
+    if land_type == "country":
+        return resorts_buttons(land_name)
+    raise ValueError("Unknown next step")
+
+
+def get_resort_info(resort_name):
+    resort = Resort.objects.get(name=resort_name)
+    top_point = resort.top_point
+    height_difference = resort.height_difference()
+    blue = resort.slopes.blue_slopes
+    red = resort.slopes.red_slopes
+    black = resort.slopes.black_slopes
+    all_slopes = resort.slopes.all_slopes()
+    resort_info = (
+        f"{resort_name}\nRed: {red}, Blue: {blue}, Black: {black}, "
+        f"All: {all_slopes}\nTop: {top_point} m, "
+        f"Height difference: {height_difference} m"
+    )
+    return resort_info
 
 
 def button(update, context):
-    land = update.callback_query.data
-    if Resort.objects.filter(name=land):
-        resort = Resort.objects.get(name=land)
-        name = land
-        top_point = resort.top_point
-        height_difference = resort.height_difference()
-        blue = resort.slopes.blue_slopes
-        red = resort.slopes.red_slopes
-        black = resort.slopes.black_slopes
-        all_slopes = resort.slopes.all_slopes()
-
-        update.callback_query.message.reply_text(
-            f"{name}\nRed: {red}, Blue: {blue}, Black: {black}, "
-            f"All: {all_slopes}\nTop: {top_point} m, "
-            f"Height difference: {height_difference} m"
-        )
-        print(update)
-        return update.callback_query.delete_message()
-    keyboard = choose_buttons(update)
     query = update.callback_query
     query.answer()
+    land = query.data
+    if land.split(sep=":")[0] == "resort":
+        resort_name = land.split(sep=":")[1]
+        update.callback_query.message.reply_text(get_resort_info(resort_name))
+        return update.callback_query.delete_message()
+    keyboard = choose_buttons(land)
     reply_markup = InlineKeyboardMarkup(keyboard)
     return query.edit_message_reply_markup(reply_markup=reply_markup)
 
--- bot/keyboards.py
+++ bot/keyboards.py
@@ -11,7 +11,7 @@ def continents_buttons():
             [
                 InlineKeyboardButton(
                     _.name,
-                    callback_data=_.name,
+                    callback_data=f"continent:{_.name}",
                 )
                 for _ in page
             ]
@@ -27,18 +27,40 @@ def countries_buttons(continent_name):
     keyboard = []
     for page in paginator:
         keyboard.append(
-            [InlineKeyboardButton(_.name, callback_data=_.name) for _ in page]
+            [
+                InlineKeyboardButton(_.name, callback_data=f"country:{_.name}")
+                for _ in page
+            ]
         )
+    keyboard.append(
+        [
+            InlineKeyboardButton(
+                "Back to list of continents", callback_data="start_info:"
+            )
+        ]
+    )
     return keyboard
 
 
 def resorts_buttons(country_name):
+    continent = Continent.objects.get(countries__name=country_name).name
     paginator = Paginator(
         Resort.objects.filter(country__name=country_name).order_by("name"), 3
     )
     keyboard = []
     for page in paginator:
         keyboard.append(
-            [InlineKeyboardButton(_.name, callback_data=_.name) for _ in page]
+            [
+                InlineKeyboardButton(_.name, callback_data=f"resort:{_.name}")
+                for _ in page
+            ]
         )
+    keyboard.append(
+        [
+            InlineKeyboardButton(
+                "Back to list of countries",
+                callback_data=f"continent:{continent}",
+            )
+        ]
+    )
     return keyboard
